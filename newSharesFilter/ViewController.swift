c7ff28b9d37a6497d147eaac559671ff7274f7ee
Riley <dakimi07@gmail.com>
2022-08-05 22:10:07 +0800

    init

---
+++ newSharesFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newSharesFilter
+//
+//  Created by Riley Lai on 2022/8/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

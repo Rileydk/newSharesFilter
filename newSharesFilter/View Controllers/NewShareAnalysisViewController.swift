322ea65e7bf81513cd2e02958a7d51ce01176135
Riley <dakimi07@gmail.com>
2022-08-06 11:51:47 +0800

    add NewShareAnalysisViewController

---
+++ newSharesFilter/View Controllers/NewShareAnalysisViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewShareAnalysisViewController.swift
+//  newSharesFilter
+//
+//  Created by Riley Lai on 2022/8/6.
+//
+
+import UIKit
+
+class NewShareAnalysisViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

a5785a3ab89665b1ae6ef1fd8a2fc5685e5e0afe
Riley <dakimi07@gmail.com>
2022-08-06 12:19:22 +0800

    add IBOutlet to NewShareAnalysisVC

--- newSharesFilter/View Controllers/NewShareAnalysisViewController.swift
+++ newSharesFilter/View Controllers/NewShareAnalysisViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class NewShareAnalysisViewController: UIViewController {
+  
+  @IBOutlet weak var companyNameLabel: UILabel!
+  @IBOutlet weak var stockCodeLabel: UILabel!
+  @IBOutlet weak var marketPriceLabel: UILabel!
+  @IBOutlet weak var subscriptionPriceLabel: UILabel!
+  @IBOutlet weak var differenceLabel: UILabel!
+  @IBOutlet weak var returnRateLabel: UILabel!
+  @IBOutlet weak var analysisResultLabel: UILabel!
+  @IBOutlet weak var dateAndTimeLabel: UILabel!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  }
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
